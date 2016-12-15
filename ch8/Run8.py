9c1b45b210c0a247ed96b774d5d4d9e42871c3de
maxim.kugatov <maxim.kugatov@digitalhouse.biz>
2016-12-15 20:26:30 +0200

    finish ch8

--- ch8/Run8.py
+++ ch8/Run8.py
@@ -54,7 +54,6 @@ df['review'] = df['review'].apply(preprocessor)
 
 def tokenizer(text):
     return text.split()
-
 print(tokenizer('runners like running and thus they run'))
 
 from nltk.stem.porter import PorterStemmer
@@ -63,3 +62,107 @@ def tokenizer_porter(text):
     return [porter.stem(word) for word in text.split()]
 
 print(tokenizer_porter('runners like running and thus they run'))
+
+# import nltk
+# print(nltk.download('stopwords'))
+
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
+tokenFor = [w for w in tokenizer_porter('a runner likes running and runs a lot')[-10:] if w not in stop]
+print(tokenFor)
+
+# Logistic regression model for document classification
+X_train = df.loc[:25000, 'review'].values
+y_train = df.loc[:25000, 'sentiment'].values
+X_test = df.loc[25000:, 'review'].values
+y_test = df.loc[25000:, 'sentiment'].values
+
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf = TfidfVectorizer(strip_accents=None, lowercase=False, preprocessor=None)
+param_grid = [{'vect__ngram_range': [(1,1)],
+    'vect__stop_words': [stop, None],
+    'vect__tokenizer': [tokenizer, tokenizer_porter],
+    'clf__penalty': ['l1', 'l2'],
+    'clf__C': [1.0, 10.0, 100.0]},
+    {'vect__ngram_range': [(1,1)],
+    'vect__stop_words': [stop, None],
+    'vect__tokenizer': [tokenizer, tokenizer_porter],
+    'vect__use_idf':[False],
+    'vect__norm':[None],
+    'clf__penalty': ['l1', 'l2'],
+    'clf__C': [1.0, 10.0, 100.0]}
+]
+lr_tfidf = Pipeline([('vect', tfidf), ('clf', LogisticRegression(random_state=0))])
+gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid,
+    scoring='accuracy',
+    cv=5, verbose=1,
+    n_jobs=-1)
+gs_lr_tfidf.fit(X_train, y_train)
+print('Best parameter set: %s ' % gs_lr_tfidf.best_params_)
+
+print('CV Accuracy: %.3f' % gs_lr_tfidf.best_score_)
+clf = gs_lr_tfidf.best_estimator_
+print('Test Accuracy: %.3f'% clf.score(X_test, y_test))
+
+import numpy as np
+import re
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
+def tokenizer(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',
+    text.lower())
+    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
+    tokenized = [w for w in text.split() if w not in stop]
+    return tokenized
+
+def stream_docs(path):
+    with open(path, 'r') as csv:
+        next(csv) # skip header
+        for line in csv:
+            text, label = line[:-3], int(line[-2])
+            yield text, label
+
+print(next(stream_docs(path='/media/max/Data/tmp/movie_data.csv')))
+
+def get_minibatch(doc_stream, size):
+    docs, y = [], []
+    try:
+        for _ in range(size):
+            text, label = next(doc_stream)
+            docs.append(text)
+            y.append(label)
+    except StopIteration:
+        return None, None
+    return docs, y
+
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.linear_model import SGDClassifier
+vect = HashingVectorizer(decode_error='ignore',
+    n_features=2**21,
+    preprocessor=None,
+    tokenizer=tokenizer)
+
+clf = SGDClassifier(loss='log', random_state=1, n_iter=1)
+doc_stream = stream_docs(path='/media/max/Data/tmp/movie_data.csv')
+
+import pyprind
+pbar = pyprind.ProgBar(45)
+classes = np.array([0, 1])
+
+for _ in range(45):
+    X_train, y_train = get_minibatch(doc_stream, size=1000)
+    if not X_train:
+        break
+    X_train = vect.transform(X_train)
+    clf.partial_fit(X_train, y_train, classes=classes)
+    pbar.update()
+
+X_test, y_test = get_minibatch(doc_stream, size=5000)
+X_test = vect.transform(X_test)
+print('Accuracy: %.3f' % clf.score(X_test, y_test))
+
+clf = clf.partial_fit(X_test, y_test)
